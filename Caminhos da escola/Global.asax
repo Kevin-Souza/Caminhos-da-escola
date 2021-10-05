bd545d291d24e1b10f806b6b3931a1f59d56cce7
Kevin-Souza <78280970+Kevin-Souza@users.noreply.github.com>
2021-10-04 21:15:10 -0300

    Adicionar arquivos de projeto.

---
+++ Caminhos da escola/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Caminhos_da_escola.MvcApplication" Language="C#" %>
---
+++ Caminhos da escola/Models/Aluno.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Caminhos_da_escola.Models
+{
+    public class Aluno
+    {
+        public long Alunoid { get; set; }
+        public string Nome { get; set; }
+        public int Matricula { get; set; }
+        public string Curso { get; set; }
+        public string Semestre { get; set; }
+    }
+}
